c91223ebaa3c32278429a8a3e20beb7281d83581
Tatiana Leitao <tatiana_felisberto_@hotmail.com>
2023-04-27 20:36:35 +0100

    - added api key

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,22 @@
 // unsplash Api
-const count = 10;
 
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+console.log("hello world");
+
+const count = 10;
+const apiKey = "XjH1BNtLqY5aFKqD_wj0CSp5kikN6_IdGF8R5fhO3fk";
+const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
+
+// Get photos from unsplash Api
+
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    //catch error here
+  }
+}
+
+//on load
+getPhotos();
